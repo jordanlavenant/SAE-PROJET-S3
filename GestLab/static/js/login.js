c66031db75c164b7f5ed448a0d2669c652bd87d0
leo-lucidor <leo.lucidor@etu.univ-orleans.fr>
2023-10-25 00:06:20 +0200

    fin de la base de code + fin login page

--- GestLab/static/js/login.js
+++ GestLab/static/js/login.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
     let estAfficherMDP = false;
     const btnChangerMDP = document.getElementById("btn-changer-mdp");
     const btnChangerMail = document.getElementById("btn-changer-mail");
+    const btnRetour = document.getElementById("btn-retour");
     const lienDeco = document.getElementById("seDeco");
     const lienAccueil = document.getElementById("lien-accueil");
     const formChangerMDP = document.getElementById("ChangerMDPForm");
@@ -12,51 +13,46 @@ document.addEventListener("DOMContentLoaded", function() {
     
     // Définir la fonction pour masquer l'élément
     function masquerMDP() {
-        if(estAfficherMail){
-            formChangerMail.style.display = "none";
+        if(!estAfficher) {
+            lienDeco.style.display = "none";
+            lienAccueil.style.display = "none";
+            btnChangerMDP.style.display = "none";
+            btnChangerMail.style.display = "none";
+            btnRetour.style.display = "block";
             formChangerMDP.style.display = "flex";
-            estAfficherMail = false;
-            estAfficherMDP = true;
-        } else {
-            if(!estAfficher) {
-                lienDeco.style.display = "none";
-                lienAccueil.style.display = "none";
-                formChangerMDP.style.display = "flex";
-                estAfficherMDP = true;
-            } else {
-                lienDeco.style.display = "block";
-                lienAccueil.style.display = "block";
-                formChangerMDP.style.display = "none";
-                estAfficherMDP = false;
-            }
-            estAfficher = !estAfficher;
+            estAfficher = true;
         }
     }
 
     // Définir la fonction pour masquer l'élément
     function masquerMail() {
-        if(estAfficherMDP){
-            formChangerMDP.style.display = "none";
-            formChangerMail.style.display = "flex";
-            estAfficherMDP = false;
-            estAfficherMail = true;
-        } else {
             if(!estAfficher) {
                 lienDeco.style.display = "none";
                 lienAccueil.style.display = "none";
+                btnChangerMDP.style.display = "none";
+                btnChangerMail.style.display = "none";
+                btnRetour.style.display = "block";
                 formChangerMail.style.display = "flex";
-                estAfficherMail = true;
-            } else {
-                lienDeco.style.display = "block";
-                lienAccueil.style.display = "block";
-                formChangerMail.style.display = "none";
-                estAfficherMail = false;
-            }
-            estAfficher = !estAfficher;
-        }
+                estAfficher = true;
+            } 
+    }
+
+    // Définir la fonction pour masquer l'élément
+    function masquerTout() {
+        formChangerMDP.style.display = "none";
+        formChangerMail.style.display = "none";
+        btnRetour.style.display = "none";
+        lienDeco.style.display = "block";
+        lienAccueil.style.display = "block";
+        btnChangerMDP.style.display = "block";
+        btnChangerMail.style.display = "block";
+        estAfficher = false;
+        estAfficherMail = false;
+        estAfficherMDP = false;
     }
 
     // Ajouter un écouteur d'événement sur le bouton pour appeler la fonction masquerElement lors du clic
     btnChangerMDP.addEventListener("click", masquerMDP);
     btnChangerMail.addEventListener("click", masquerMail);
+    btnRetour.addEventListener("click", masquerTout);
 });
