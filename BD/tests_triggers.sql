1bd547f41db474ba5eafd7fe1d811f89ad4683f8
Lallier Anna <o22210239@info23-07.iut45.univ-orleans.fr>
2023-10-25 14:23:59 +0200

    Adaptation des anciens triggers (update et delete), modification automatique des objets en stock du fournisseur

--- BD/tests_triggers.sql
+++ BD/tests_triggers.sql
@@ -1,5 +1,24 @@
 --emailUtilisateurUnique
 
 INSERT INTO UTILISATEUR (idStatut, nom, prenom, email, motDePasse) VALUES 
-(4, 'Dupont', 'Dupond', 'john.doe@example.com', 'mdp');
+(1, 'Dupont', 'Dupond', 'john.doe@example.com', 'mdp');
 
+UPDATE UTILISATEUR SET email = 'john.doe@example.com' WHERE idUtilisateur = 2 ;
+
+--insereSommeCommandeUpdate
+SELECT * FROM DEMANDE ;
+
+UPDATE AJOUTERMATERIEL SET quantite = 4 WHERE idDemande = 2 ;
+
+DELETE FROM AJOUTERMATERIEL WHERE idMateriel = 2 AND idDemande = 1 ;
+
+--verificationStockFournisseur
+SELECT * FROM MATERIELFOURNISSEUR ;
+
+UPDATE AJOUTERMATERIEL SET quantite = 100 WHERE idDemande = 1 AND idMateriel = 1;
+
+UPDATE AJOUTERMATERIEL SET quantite = 5 WHERE idDemande = 1 AND idMateriel = 1;
+
+UPDATE AJOUTERMATERIEL SET quantite = 15 WHERE idDemande = 1 AND idMateriel = 1;
+
+DELETE FROM AJOUTERMATERIEL WHERE idMateriel = 1 AND idDemande = 1 ;
--- BD/trigger.sql
+++ BD/trigger.sql
@@ -1,5 +1,5 @@
 delimiter |
-create or replace TRIGGER emailUtilisateurUnique before insert on UTILISATEUR for each row
+create or replace TRIGGER emailUtilisateurUniqueInsert before insert on UTILISATEUR for each row
 begin
     declare compteur int;
     declare mes varchar(255);
@@ -11,6 +11,18 @@ begin
 end |
 delimiter ;
 
+delimiter |
+create or replace TRIGGER emailUtilisateurUniqueUpdate before update on UTILISATEUR for each row
+begin
+    declare compteur int;
+    declare mes varchar(255);
+    SELECT COUNT(*) INTO compteur from UTILISATEUR WHERE email = new.email;
+    if compteur > 0 then
+        set mes = concat("L'email ", new.email, " est déjà utilisé.");
+        signal SQLSTATE '45000' set MESSAGE_TEXT = mes;
+    end if;
+end |
+delimiter ;
 
 
 delimiter |
@@ -33,7 +45,7 @@ delimiter ;
 
 
 delimiter |
-create or replace TRIGGER insereSommeCommande after insert on AJOUTERMATERIEL for each row
+create or replace TRIGGER insereSommeCommandeInsert after insert on AJOUTERMATERIEL for each row
 begin
     declare mes varchar(255);
     declare idM int ;
@@ -60,6 +72,65 @@ begin
     UPDATE DEMANDE SET prixTotalDemande = sommePrix WHERE idDemande = new.idDemande ;
 end |
 delimiter ;
+
+delimiter |
+create or replace TRIGGER insereSommeCommandeUpdate after update on AJOUTERMATERIEL for each row
+begin
+    declare mes varchar(255);
+    declare idM int ;
+    declare idF int ;
+    declare qte int ;
+    declare sommePrix float default 0;
+    declare prixIndividuel float;
+    declare fini boolean default false ;
+
+    declare produits cursor for 
+        SELECT idMateriel, idFournisseur, quantite FROM AJOUTERMATERIEL WHERE idDemande = new.idDemande;
+        
+    declare continue handler for not found set fini = true ;
+    open produits ;
+    while not fini do
+        fetch produits into idM, idF, qte ;
+        if not fini then
+            SELECT recuperePrixMateriel(idM, idF, qte) into prixIndividuel ;
+            SET sommePrix = sommePrix + prixIndividuel ;
+        end if ;
+    end while ;
+    close produits ;
+
+    UPDATE DEMANDE SET prixTotalDemande = sommePrix WHERE idDemande = new.idDemande ;
+end |
+delimiter ;
+
+delimiter |
+create or replace TRIGGER insereSommeCommandeDelete after delete on AJOUTERMATERIEL for each row
+begin
+    declare mes varchar(255);
+    declare idM int ;
+    declare idF int ;
+    declare qte int ;
+    declare sommePrix float default 0;
+    declare prixIndividuel float;
+    declare fini boolean default false ;
+
+    declare produits cursor for 
+        SELECT idMateriel, idFournisseur, quantite FROM AJOUTERMATERIEL WHERE idDemande = old.idDemande;
+        
+    declare continue handler for not found set fini = true ;
+    open produits ;
+    while not fini do
+        fetch produits into idM, idF, qte ;
+        if not fini then
+            SELECT recuperePrixMateriel(idM, idF, qte) into prixIndividuel ;
+            SET sommePrix = sommePrix + prixIndividuel ;
+        end if ;
+    end while ;
+    close produits ;
+
+    UPDATE DEMANDE SET prixTotalDemande = sommePrix WHERE idDemande = old.idDemande ;
+end |
+delimiter ;
+
 
 
 delimiter |
@@ -90,5 +161,46 @@ begin
 end |
 delimiter ;
 
-SELECT demandesEnAttente() ;
 
+
+
+delimiter |
+create or replace TRIGGER verificationStockFournisseurInsert before insert on AJOUTERMATERIEL for each row
+begin
+    declare mes varchar(255);
+    declare stock int ;
+
+    SELECT stockFournisseur INTO stock FROM MATERIELFOURNISSEUR WHERE idMateriel = new.idMateriel AND idFournisseur = new.idFournisseur ; 
+        
+    if stock >= new.quantite then 
+        UPDATE MATERIELFOURNISSEUR SET stockFournisseur = (stockFournisseur-new.quantite) WHERE idMateriel = new.idMateriel AND idFournisseur = new.idFournisseur ;
+    else set mes = concat("La quantité entrée (", new.quantite, ") est supérieure à celle que le fournisseur propose (", stock, ").");
+        signal SQLSTATE '45000' set MESSAGE_TEXT = mes;
+    end if;
+end |
+delimiter ;
+
+delimiter |
+create or replace TRIGGER verificationStockFournisseurUpdate before update on AJOUTERMATERIEL for each row
+begin
+    declare mes varchar(255);
+    declare stock int ;
+    declare quantiteInitiale int ;
+
+    SELECT stockFournisseur INTO stock FROM MATERIELFOURNISSEUR WHERE idMateriel = new.idMateriel AND idFournisseur = new.idFournisseur ; 
+    SELECT quantite INTO quantiteInitiale FROM AJOUTERMATERIEL WHERE idMateriel = new.idMateriel AND idFournisseur = new.idFournisseur ; 
+        
+    if stock >= new.quantite then 
+        UPDATE MATERIELFOURNISSEUR SET stockFournisseur = (stockFournisseur+quantiteInitiale-new.quantite) WHERE idMateriel = new.idMateriel AND idFournisseur = new.idFournisseur ;
+    else set mes = concat("La quantité entrée (", new.quantite, ") est supérieure à celle que le fournisseur propose (", stock, ").");
+        signal SQLSTATE '45000' set MESSAGE_TEXT = mes;
+    end if;
+end |
+delimiter ;
+
+delimiter |
+create or replace TRIGGER verificationStockFournisseurDelete before delete on AJOUTERMATERIEL for each row
+begin    
+    UPDATE MATERIELFOURNISSEUR SET stockFournisseur = (stockFournisseur+old.quantite) WHERE idMateriel = old.idMateriel AND idFournisseur = old.idFournisseur ;
+end |
+delimiter ;
