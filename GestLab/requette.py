2bec1fcf3147c98965acf499eaa3cd26d21d1ee1
leo-lucidor <leo.lucidor@etu.univ-orleans.fr>
2023-10-26 12:32:28 +0200

    correction bug lancement

--- GestLab/requette.py
+++ GestLab/requette.py
@@ -373,6 +373,9 @@ def get_all_information_utilisateur_with_id(cnx,id):
         result = cnx.execute(text("select nom,prenom,email,nomStatut from UTILISATEUR natural join STATUT where idUtilisateur = " + str(id) + ";"))
         for row in result:
             print(row)
+    except:
+        print("erreur de l'id")
+        raise
 
 def get_all_information_to_Materiel_with_id(cnx, id):
     try:
