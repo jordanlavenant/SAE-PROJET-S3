9501a9d7cf76051fc606a04591add0ebf25da2a3
Lallier Anna <o22210239@info23-07.iut45.univ-orleans.fr>
2023-10-26 13:58:43 +0200

    Suppression des triggers et fonctions devenues non pertientes avec la modification des tables

--- BD/tests_triggers.sql
+++ BD/tests_triggers.sql
@@ -1,32 +1,13 @@
 --emailUtilisateurUnique
 
 INSERT INTO UTILISATEUR (idStatut, nom, prenom, email, motDePasse) VALUES 
-(1, 'Dupont', 'Dupond', 'john.doe@example.com', 'mdp');
+(3, 'Dupont', 'Dupond', 'admin@example.com', 'mdp');
 
-UPDATE UTILISATEUR SET email = 'john.doe@example.com' WHERE idUtilisateur = 2 ;
-
---insereSommeCommandeUpdate
-SELECT * FROM DEMANDE ;
-
-UPDATE AJOUTERMATERIEL SET quantite = 4 WHERE idDemande = 2 ;
-
-DELETE FROM AJOUTERMATERIEL WHERE idMateriel = 2 AND idDemande = 1 ;
-
---verificationStockFournisseur
-SELECT * FROM MATERIELFOURNISSEUR ;
-
-UPDATE AJOUTERMATERIEL SET quantite = 100 WHERE idDemande = 1 AND idMateriel = 1;
-
-UPDATE AJOUTERMATERIEL SET quantite = 5 WHERE idDemande = 1 AND idMateriel = 1;
-
-UPDATE AJOUTERMATERIEL SET quantite = 15 WHERE idDemande = 1 AND idMateriel = 1;
-
-DELETE FROM AJOUTERMATERIEL WHERE idMateriel = 1 AND idDemande = 1 ;
+UPDATE UTILISATEUR SET email = 'admin@example.com' WHERE idUtilisateur = 2 ;
 
 --modifsSurDemande
-INSERT INTO AJOUTERMATERIEL (idDemande, idMateriel, idFournisseur, quantite) VALUES (2, 4, 3, 1);
 
-UPDATE AJOUTERMATERIEL SET quantite = 1 WHERE idDemande = 2 ;
+UPDATE DEMANDE SET descriptionDemande = 'blablabla' WHERE idDemande = 2 ;
 
 --modificationStockLabo
 UPDATE BONCOMMANDE SET idEtat = 3 WHERE idDemande = 2 ;
--- BD/trigger.sql
+++ BD/trigger.sql
@@ -1,5 +1,5 @@
 delimiter |
-create or replace TRIGGER modifsSurDemandeInsert before insert on AJOUTERMATERIEL for each row
+create or replace TRIGGER modifsSurDemandeUpdate before update on DEMANDE for each row
 begin
     declare presente int;
     declare mes varchar(255);
@@ -7,26 +7,12 @@ begin
     SELECT COUNT(ifnull(DEMANDE.idDemande, 0)) into presente FROM DEMANDE JOIN BONCOMMANDE ON DEMANDE.idDemande = BONCOMMANDE.idDemande WHERE DEMANDE.idDemande = new.idDemande ;
 
     if presente > 0 then
-        set mes = concat("Le matériel ne peut être ajouté à la demande (id demande : ", new.idDemande,") car celle-ci déjà associée à un bon de commande.");
+        set mes = concat("La modification ne peut être effectuée sur la demande (id demande : ", new.idDemande,") car celle-ci est associée à un bon de commande.");
         signal SQLSTATE '45000' set MESSAGE_TEXT = mes;
     end if;
 end |
 delimiter ;
 
-delimiter |
-create or replace TRIGGER modifsSurDemandeUpdate before update on AJOUTERMATERIEL for each row
-begin
-    declare presente int;
-    declare mes varchar(255);
-
-    SELECT COUNT(ifnull(DEMANDE.idDemande, 0)) into presente FROM DEMANDE JOIN BONCOMMANDE ON DEMANDE.idDemande = BONCOMMANDE.idDemande WHERE DEMANDE.idDemande = new.idDemande ;
-
-    if presente > 0 then
-        set mes = concat("La modification ne peut être effectuée sur la demande (id demande : ", new.idDemande,") car celle-ci déjà associée à un bon de commande.");
-        signal SQLSTATE '45000' set MESSAGE_TEXT = mes;
-    end if;
-end |
-delimiter ;
 
 delimiter |
 create or replace TRIGGER emailUtilisateurUniqueInsert before insert on UTILISATEUR for each row
@@ -55,25 +41,6 @@ end |
 delimiter ;
 
 
-delimiter |
-CREATE OR REPLACE function recupereSommeActuelle(id int) returns float
-BEGIN
-declare sommeActuelle float;
-SELECT prixTotalDemande INTO sommeActuelle FROM DEMANDE WHERE idDemande = id ;
-return sommeActuelle;
-end |
-delimiter ;
-
-delimiter |
-CREATE OR REPLACE function recuperePrixMateriel(idM int, idF int, qte int) returns float
-BEGIN
-declare prix float;
-SELECT prixMateriel INTO prix FROM MATERIELFOURNISSEUR WHERE idMateriel = idM and idFournisseur = idF ;
-return prix*qte;
-end |
-delimiter ;
-
-
 
 delimiter |
 CREATE OR REPLACE function recupereStockLabo(idM int) returns int
@@ -81,6 +48,35 @@ BEGIN
 declare stock float;
 SELECT quantiteLaboratoire INTO stock FROM STOCKLABORATOIRE WHERE idMateriel = idM ;
 return stock;
+end |
+delimiter ;
+
+
+delimiter |
+create or replace FUNCTION demandesEnAttente() returns varchar(255)
+begin
+    declare id int ;
+    declare listeDemandes varchar(255) default '';
+    declare fini boolean default false ;
+
+    declare idDemandes cursor for 
+        SELECT DEMANDE.idDemande FROM DEMANDE JOIN BONCOMMANDE ON DEMANDE.idDemande = BONCOMMANDE.idDemande WHERE BONCOMMANDE.idEtat = 1 ;
+
+    declare continue handler for not found set fini = true ;
+    open idDemandes ;
+    while not fini do
+        fetch idDemandes into id ;
+        if not fini then 
+            if listeDemandes = '' then 
+                SET listeDemandes = id ;
+            else 
+                SET listeDemandes = concat(listeDemandes, ", ", id) ;
+            end if ;
+        end if ;
+    end while ;
+    close idDemandes ;
+
+    return listeDemandes ;
 end |
 delimiter ;
 
@@ -152,166 +148,5 @@ BEGIN
         end while ;
         close produits ;
     end if;
-end |
-delimiter ;
-
-delimiter |
-create or replace TRIGGER insereSommeCommandeInsert after insert on AJOUTERMATERIEL for each row
-begin
-    declare mes varchar(255);
-    declare idM int ;
-    declare idF int ;
-    declare qte int ;
-    declare sommePrix float default 0;
-    declare prixIndividuel float;
-    declare fini boolean default false ;
-
-    declare produits cursor for 
-        SELECT idMateriel, idFournisseur, quantite FROM AJOUTERMATERIEL WHERE idDemande = new.idDemande;
-        
-    declare continue handler for not found set fini = true ;
-    open produits ;
-    while not fini do
-        fetch produits into idM, idF, qte ;
-        if not fini then
-            SELECT recuperePrixMateriel(idM, idF, qte) into prixIndividuel ;
-            SET sommePrix = sommePrix + prixIndividuel ;
-        end if ;
-    end while ;
-    close produits ;
-
-    UPDATE DEMANDE SET prixTotalDemande = sommePrix WHERE idDemande = new.idDemande ;
-end |
-delimiter ;
-
-delimiter |
-create or replace TRIGGER insereSommeCommandeUpdate after update on AJOUTERMATERIEL for each row
-begin
-    declare mes varchar(255);
-    declare idM int ;
-    declare idF int ;
-    declare qte int ;
-    declare sommePrix float default 0;
-    declare prixIndividuel float;
-    declare fini boolean default false ;
-
-    declare produits cursor for 
-        SELECT idMateriel, idFournisseur, quantite FROM AJOUTERMATERIEL WHERE idDemande = new.idDemande;
-        
-    declare continue handler for not found set fini = true ;
-    open produits ;
-    while not fini do
-        fetch produits into idM, idF, qte ;
-        if not fini then
-            SELECT recuperePrixMateriel(idM, idF, qte) into prixIndividuel ;
-            SET sommePrix = sommePrix + prixIndividuel ;
-        end if ;
-    end while ;
-    close produits ;
-
-    UPDATE DEMANDE SET prixTotalDemande = sommePrix WHERE idDemande = new.idDemande ;
-end |
-delimiter ;
-
-delimiter |
-create or replace TRIGGER insereSommeCommandeDelete after delete on AJOUTERMATERIEL for each row
-begin
-    declare mes varchar(255);
-    declare idM int ;
-    declare idF int ;
-    declare qte int ;
-    declare sommePrix float default 0;
-    declare prixIndividuel float;
-    declare fini boolean default false ;
-
-    declare produits cursor for 
-        SELECT idMateriel, idFournisseur, quantite FROM AJOUTERMATERIEL WHERE idDemande = old.idDemande;
-        
-    declare continue handler for not found set fini = true ;
-    open produits ;
-    while not fini do
-        fetch produits into idM, idF, qte ;
-        if not fini then
-            SELECT recuperePrixMateriel(idM, idF, qte) into prixIndividuel ;
-            SET sommePrix = sommePrix + prixIndividuel ;
-        end if ;
-    end while ;
-    close produits ;
-
-    UPDATE DEMANDE SET prixTotalDemande = sommePrix WHERE idDemande = old.idDemande ;
-end |
-delimiter ;
-
-
-
-delimiter |
-create or replace FUNCTION demandesEnAttente() returns varchar(255)
-begin
-    declare id int ;
-    declare listeDemandes varchar(255) default '';
-    declare fini boolean default false ;
-
-    declare idDemandes cursor for 
-        SELECT DEMANDE.idDemande FROM DEMANDE JOIN BONCOMMANDE ON DEMANDE.idDemande = BONCOMMANDE.idDemande WHERE BONCOMMANDE.idEtat = 1 ;
-
-    declare continue handler for not found set fini = true ;
-    open idDemandes ;
-    while not fini do
-        fetch idDemandes into id ;
-        if not fini then 
-            if listeDemandes = '' then 
-                SET listeDemandes = id ;
-            else 
-                SET listeDemandes = concat(listeDemandes, ", ", id) ;
-            end if ;
-        end if ;
-    end while ;
-    close idDemandes ;
-
-    return listeDemandes ;
-end |
-delimiter ;
-
-
-
-
-delimiter |
-create or replace TRIGGER verificationStockFournisseurInsert before insert on AJOUTERMATERIEL for each row
-begin
-    declare mes varchar(255);
-    declare stock int ;
-
-    SELECT stockFournisseur INTO stock FROM MATERIELFOURNISSEUR WHERE idMateriel = new.idMateriel AND idFournisseur = new.idFournisseur ; 
-        
-    if stock >= new.quantite then 
-        UPDATE MATERIELFOURNISSEUR SET stockFournisseur = (stockFournisseur-new.quantite) WHERE idMateriel = new.idMateriel AND idFournisseur = new.idFournisseur ;
-    else set mes = concat("La quantité entrée (", new.quantite, ") est supérieure à celle que le fournisseur propose (", stock, ").");
-        signal SQLSTATE '45000' set MESSAGE_TEXT = mes;
-    end if;
-end |
-delimiter ;
-
-delimiter |
-create or replace TRIGGER verificationStockFournisseurUpdate before update on AJOUTERMATERIEL for each row
-begin
-    declare mes varchar(255);
-    declare stock int ;
-    declare quantiteInitiale int ;
-
-    SELECT stockFournisseur INTO stock FROM MATERIELFOURNISSEUR WHERE idMateriel = new.idMateriel AND idFournisseur = new.idFournisseur ; 
-    SELECT quantite INTO quantiteInitiale FROM AJOUTERMATERIEL WHERE idMateriel = new.idMateriel AND idFournisseur = new.idFournisseur ; 
-        
-    if stock >= new.quantite then 
-        UPDATE MATERIELFOURNISSEUR SET stockFournisseur = (stockFournisseur+quantiteInitiale-new.quantite) WHERE idMateriel = new.idMateriel AND idFournisseur = new.idFournisseur ;
-    else set mes = concat("La quantité entrée (", new.quantite, ") est supérieure à celle que le fournisseur propose (", stock, ").");
-        signal SQLSTATE '45000' set MESSAGE_TEXT = mes;
-    end if;
-end |
-delimiter ;
-
-delimiter |
-create or replace TRIGGER verificationStockFournisseurDelete before delete on AJOUTERMATERIEL for each row
-begin    
-    UPDATE MATERIELFOURNISSEUR SET stockFournisseur = (stockFournisseur+old.quantite) WHERE idMateriel = old.idMateriel AND idFournisseur = old.idFournisseur ;
 end |
 delimiter ;
