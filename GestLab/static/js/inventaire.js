afd21d0d052a526973e751cd6ec97357288d32d4
jordanlavenant <jordan.lavenant@etu.univ-orleans.fr>
2023-11-07 14:31:50 +0100

    Mise à jour de la bordure en fonction des alertes de chaque items

--- GestLab/static/js/inventaire.js
+++ GestLab/static/js/inventaire.js
@@ -1,16 +1,15 @@
-// const item = document.getElementById("lenItem");
-// const value = parseInt(item.className)
-// console.log(value)
+const item = document.getElementById("lenItem");
+const value = parseInt(item.className)
 
-
-// for (var i=0;i<value;i++) {
-//     console.log(i)
-//     const item = document.getElementById(i);
-//     const alertsValue = document.getElementById("alerts_count_"+i);
-//     console.log(parseInt(alertsValue.className))
-//     // if (alertsValue.value > 0) {
-//     //     item.style.border_left = "15px #2EDD94 solid";
-//     // } else {
-//     //     item.style.border_right = "15px #d35252 solid";
-//     // }
-// }
+for (var i=1;i<value;i++) {
+    console.log(i)
+    const item = document.getElementById(`${i}`);
+    const alert = document.getElementById(`alerts_count_${i}`);
+    console.log(alert)
+    // const alertsValue = document.getElementById(`alerts_count_2`);
+    if (alert == null) {
+        item.style.borderLeft = "#2EDD94 solid 15px";
+    } else {
+        item.style.borderLeft = "#d35252 solid 15px";
+    }
+}
