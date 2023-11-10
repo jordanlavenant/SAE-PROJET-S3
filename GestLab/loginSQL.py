0650508cdb29db7658fdad74bd5c3e12c01186eb
leo-lucidor <leo.lucidor@etu.univ-orleans.fr>
2023-11-10 10:27:59 +0100

    passage bd9

--- GestLab/loginSQL.py
+++ GestLab/loginSQL.py
@@ -1,7 +1,7 @@
 login = "blandeau" # pip install pymysql
 passwd = "Loulou34230."
 serveur= "mysql-blandeau.alwaysdata.net"
-bd = "blandeau_gestlabbd8"
+bd = "blandeau_gestlabbd9"
 
 def getLogin():
     return login
