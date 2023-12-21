b402af619376d0b801f0ef3b030e3616374a5bb7
Anna Lallier <anlasco231@gmail.com>
2023-12-21 16:35:43 +0100

    Résolution du duplicata Rangement dans requetebd5

--- GestLab/requetebd5.py
+++ GestLab/requetebd5.py
@@ -1460,6 +1460,17 @@ class Rangement:
                 print("Erreur lors de la récupération de l'id de l'endroit")
                 raise
 
+    class Insert :
+        def insere_rangement(cnx, id_endroit, position) :
+            try :
+                print(text("INSERT INTO RANGEMENT (idEndroit, position) VALUES (" + str(id_endroit) + ", '" + str(position) + "');"))
+                cnx.execute(text("INSERT INTO RANGEMENT (idEndroit, position) VALUES (" + str(id_endroit) + ", '" + str(position) + "');"))
+                cnx.commit()
+                return True
+            except :
+                print("Erreur lors de l'insertion de l'endroit")
+                return False
+
 
 class Commande :
 
@@ -1816,17 +1827,6 @@ class Endroit :
                 print("Erreur lors de l'insertion de l'endroit")
                 return False
             
-class Rangement :
-    class Insert :
-        def insere_rangement(cnx, id_endroit, position) :
-            try :
-                print(text("INSERT INTO RANGEMENT (idEndroit, position) VALUES (" + str(id_endroit) + ", '" + str(position) + "');"))
-                cnx.execute(text("INSERT INTO RANGEMENT (idEndroit, position) VALUES (" + str(id_endroit) + ", '" + str(position) + "');"))
-                cnx.commit()
-                return True
-            except :
-                print("Erreur lors de l'insertion de l'endroit")
-                return False
             
 # def get_all_information_to_Materiel(cnx, nomcat=None):
 #     my_list = []
