5959d33f321915469342799e7c5254852b24b775
jordanlavenant <jordan.lavenant@etu.univ-orleans.fr>
2024-02-19 16:01:09 +0100

    template alertes

--- GestLab/static/js/inventaire.js
+++ GestLab/static/js/inventaire.js
@@ -1,8 +1,7 @@
-// const alerts = document.querySelectorAll('.alerts_count').forEach(alert => {
-//     let parent = alert;
-//     // On remonte 5 fois pour arriver à la div parent #item
-//     for (let i = 0; i < 5 ; i++){
-//         parent = parent.parentElement;
-//     }
-//     parent.style.borderLeft = "15px solid #d35252";
-// });
+const alerts = document.querySelectorAll('.alerts_count').forEach(alert => {
+    let parent = alert;
+    for (let i = 0 ; i < 5 ; i++) {
+        parent = parent.parentElement;
+    }
+    parent.style.borderLeft = "15px solid #d35252";
+});
