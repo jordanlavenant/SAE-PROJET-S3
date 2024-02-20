cc26a312f164d22e6d930fb37fb2e0c209495598
Anna Lallier <anlasco231@gmail.com>
2024-02-20 10:04:05 +0100

    Merge de toutes les branches en rapport avec le CSV

--- GestLab/loginSQL.py
+++ GestLab/loginSQL.py
@@ -1,7 +1,7 @@
 login = "blandeau" # pip install pymysql
 passwd = "Loulou34230."
 serveur= "mysql-blandeau.alwaysdata.net"
-bd = "blandeau_gestlab21"
+bd = "blandeau_gestlab19"
 
 def getLogin():
     return login
