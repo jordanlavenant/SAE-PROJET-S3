a1104260b59558e896602a957b5b7ad77fb55856
Anna Lallier <anlasco231@gmail.com>
2024-02-21 09:58:59 +0100

    switch theme sombre/theme light efficace

--- GestLab/loginSQL.py
+++ GestLab/loginSQL.py
@@ -1,7 +1,7 @@
-login = "blandeau" # pip install pymysql
-passwd = "Loulou34230."
-serveur= "mysql-blandeau.alwaysdata.net"
-bd = "blandeau_gestlab22"
+login = "lallier" # pip install pymysql
+passwd = "Humpt1Dumpt1"
+serveur= "servinfo-maria"
+bd = "DBlallier"
 
 def getLogin():
     return login
