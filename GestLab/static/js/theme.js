0292ac225968c601e0c93c117345792ae2bf1a75
Anna Lallier <anlasco231@gmail.com>
2024-02-21 14:27:27 +0100

    Ajax

--- GestLab/static/js/theme.js
+++ GestLab/static/js/theme.js
@@ -7,15 +7,24 @@ if (localStorage.getItem("theme")) {
 function toggleTheme(userId) {
 	console.log(userId);
     const body = document.body;
+	const url = "/update-theme/" + userId;
 
     if (body.classList.toggle("dark")) {
         applyTheme("dark");
         localStorage.setItem("theme", "dark");
+		$.ajax({
+			url: url,
+			type: 'POST',
+			success: function(response) {
+				console.log(response);
+			}
+		});
     } else {
         applyTheme("default");
         localStorage.setItem("theme", "default");
     }
 }
+
 
 function applyTheme(theme) {
     const body = document.body;
